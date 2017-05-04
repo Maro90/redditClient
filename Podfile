0ff45abc3627cda487a99883af1af0a46775d6c8
Mauro Gonzalez <mauro.d.gonzalez90@gmail.com>
2017-05-04 11:53:16 -0300

    Add: Initial Commit

---
+++ Podfile
@@ -0,0 +1,9 @@
+source 'https://github.com/CocoaPods/Specs.git'
+platform :ios, '10.0'
+workspace 'redditClient.xcworkspace'
+project 'redditClient.xcodeproj'
+use_frameworks!
+
+target 'redditClient' do
+    pod 'Alamofire', '~> 4.4'
+end
---
+++ redditClient/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  redditClient
+//
+//  Created by Mauro Gonzalez on 5/4/17.
+//  Copyright © 2017 Mauro Gonzalez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
